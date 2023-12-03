9fadf56b8bf6c1573451b3cdf5f0d7b3dd1c35bf
Kweku7k <mr.adumatta@gmail.com>
2023-12-03 10:36:05 +0000

    push to production

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Delete the previous environment
+sudo rm -r env
+
+# Set the name of the virtual environment
+venv_name="env"
+
+# Create a new virtual environment
+python3 -m venv $venv_name
+
+# Activate the virtual environment
+source $venv_name/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+#Restart the forms application
+# sudo systemctl restart presto.service
+
+# Deactivate the virtual environment
+#deactivate
+
